bc1baf3e75b5d4c888444e93ae944239605a2f3c
Thananon <thananon@digio.co.th>
2020-12-09 12:00:17 +0700

    Navigate app to the location screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -26,6 +26,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.long;
     NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen();
+    }));
     print(weatherData);
   }
 
