0f07360611349db245416d6be477ac143f15591e
Thananon <thananon@digio.co.th>
2020-12-09 11:51:32 +0700

    Move network part to networking file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,10 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
+
+
+const apiKey = '24962586e02c22b6f32c6d7104a847f6';
 
 
 class LoadingScreen extends StatefulWidget {
@@ -11,34 +14,29 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   Position position;
-  void getLocation() async{
+  double latitude;
+  double longitude;
+
+   void getLocationAndData() async{
     Location location = Location();
     await location.getCurrentLocation();
-    print('long'+location.long.toStringAsFixed(3)+' lat:'+location.lat.toStringAsFixed(3));
+    latitude = location.lat;
+    longitude = location.long;
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    print(weatherData);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocation();
+    getLocationAndData();
   }
 
-  Future<void> getData() async{
-    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=24962586e02c22b6f32c6d7104a847f6');
-    print('this function still work');
-    print(response.body);
-    if(response.statusCode ==200){
-      String data = response.body;
-    }else{
-      print(response.statusCode);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    print('called getData');
-    getData();
     return Scaffold(
 
     );
